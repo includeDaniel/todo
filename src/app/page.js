5107d03870b6a98fd237d45620cf9433ac59aee5
Daniel Silva <danielzcomputacao@gmail.com>
2023-05-21 15:49:49 -0300

    bg error solved

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import { Body } from '../components/Body'
 
 export default function Home() {
 	return (
-		<div className='w-screen flex items-center flex-col h-screen bg-slate-400'>
+		<div className='w-full flex items-center flex-col min-h-screen max-h-max  bg-slate-400'>
 			<TodoTitle>My Tasks</TodoTitle>
 			<Body />
 		</div>
