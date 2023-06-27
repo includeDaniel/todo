012cdc820e8838a14214248494f0151d92e16802
Daniel Silva <danielzcomputacao@gmail.com>
2023-06-27 18:29:20 -0300

    testing useTodo hook  using jest

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,48 +1,52 @@
 import { memo } from "react";
 
-const TodoItems = ({ action, state, toggleStatus, UpdateList, removeItem }) => {
-    return action.filter(state).map((i) => (
-        <ul
-            className=" w-full border-2 border-white"
-            style={{ listStyleType: "none" }}
-        >
-            <div
-                key={i.id}
-                className="w-full h-16 border-2 boreder-white flex items-center justify-between"
+const TodoItems = memo(
+    ({ action, state, toggleStatus, UpdateList, removeItem }) => {
+        return action.filter(state).map((i) => (
+            <ul
+                className=" w-full border-2 border-white"
+                style={{ listStyleType: "none" }}
             >
-                <button
-                    className="w-16 h-16 border-2 border-white flex justify-center items-center"
-                    onClick={() => toggleStatus(i.id, i.status)}
+                <div
+                    key={i.id}
+                    className="w-full h-16 border-2 boreder-white flex items-center justify-between"
                 >
-                    <div
+                    <button
+                        className="w-16 h-16 border-2 border-white flex justify-center items-center"
+                        onClick={() => toggleStatus(i.id, i.status)}
+                    >
+                        <div
+                            style={{
+                                backgroundColor:
+                                    i.status === "completed"
+                                        ? "green"
+                                        : "transparent",
+                            }}
+                            className="w-6 h-6 rounded-full border-4 border-green-700"
+                        ></div>
+                    </button>
+                    <li
+                        contenteditable="true"
+                        onBlur={(e) => UpdateList(e, i.id)}
                         style={{
-                            backgroundColor:
+                            textDecoration:
                                 i.status === "completed"
-                                    ? "green"
-                                    : "transparent",
+                                    ? "line-through"
+                                    : "none",
+                            color: i.status === "completed" ? "gray" : "white",
                         }}
-                        className="w-6 h-6 rounded-full border-4 border-green-700"
-                    ></div>
-                </button>
-                <li
-                    contenteditable="true"
-                    onBlur={(e) => UpdateList(e, i.id)}
-                    style={{
-                        textDecoration:
-                            i.status === "completed" ? "line-through" : "none",
-                        color: i.status === "completed" ? "gray" : "white",
-                    }}
-                >
-                    {i.value}
-                </li>
-                <button
-                    className="w-16 h-16 text-red-500 border-2 border-white"
-                    onClick={() => removeItem(i)}
-                >
-                    x
-                </button>
-            </div>
-        </ul>
-    ));
-};
+                    >
+                        {i.value}
+                    </li>
+                    <button
+                        className="w-16 h-16 text-red-500 border-2 border-white"
+                        onClick={() => removeItem(i)}
+                    >
+                        x
+                    </button>
+                </div>
+            </ul>
+        ));
+    }
+);
 export { TodoItems };
