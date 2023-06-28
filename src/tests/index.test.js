91b544ec9df6939f4a877fb470cbd472af75dc6d
Daniel Silva <danielzcomputacao@gmail.com>
2023-06-28 12:51:02 -0300

    testing filter and toggle and toggle all with jest

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -61,4 +61,59 @@ describe("useTodo", () => {
             items: [{ id: "123", value: "batatinha", status: "active" }],
         });
     });
+    test("should toggle a item's status in todo's list", async () => {
+        const { result } = renderHook(() => useTodo());
+        const { action } = result.current;
+
+        act(() => {
+            action.toggleStatus("123", "active");
+        });
+        expect(result.current.todo).toStrictEqual({
+            active: 0,
+            completed: 1,
+            items: [{ id: "123", value: "batatinha", status: "completed" }],
+        });
+    });
+
+    test("should filter with status in todo's list", async () => {
+        const { result } = renderHook(() => useTodo());
+        const { action } = result.current;
+
+        act(() => {
+            action.append({ id: "124", value: "batatinha2", status: "active" });
+            action.filter("all");
+        });
+        expect(result.current.todo).toStrictEqual({
+            active: 1,
+            completed: 1,
+            items: [
+                { id: "123", value: "batatinha", status: "completed" },
+                {
+                    id: "124",
+                    status: "active",
+                    value: "batatinha2",
+                },
+            ],
+        });
+    });
+    test("should toggle all status of todo's list", async () => {
+        const { result } = renderHook(() => useTodo());
+        const { action } = result.current;
+
+        act(() => {
+            action.toggleAllStatus();
+        });
+        expect(result.current.todo).toStrictEqual({
+            active: 0,
+            completed: 2,
+            items: [
+                { id: "123", value: "batatinha", status: "completed" },
+                {
+                    id: "124",
+                    status: "completed",
+                    value: "batatinha2",
+                },
+            ],
+        });
+    });
 });
