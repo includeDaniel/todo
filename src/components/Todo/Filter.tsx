e920992c325221dd227562806cd5ff761fcb9659
Daniel Silva <danielzcomputacao@gmail.com>
2023-08-02 13:18:45 -0300

    final adjust in filter buttons positions

--- src/components/Todo/Filter.tsx
+++ src/components/Todo/Filter.tsx
@@ -14,24 +14,24 @@ const Filter = ({
     clearCompleted,
 }: FilterProps) => (
     <div className="tablet:w-full tablet:h-16 cellphone:h-10 cellphone:w-80  tablet:text-sm cellphone:text-xs text-zinc-500 flex justify-between bg-slate-800 border-2 border-white">
-        <div className="tablet:mr-10 tablet:ml-5 cellphone:mr-1 cellphone:ml-2 flex items-center justify-center">
+        <div className="tablet:pr-10 tablet:pl-5 tablet:mr-0 tablet:ml-0 cellphone:mr-1 cellphone:ml-2 flex items-center justify-center">
             <span>{length} items left</span>
         </div>
         <div className="flex justify-center items-center">
             <button
-                className="tablet:p-5 cellphone:ml-3 text-zinc-500"
+                className="tablet:p-5 tablet:ml-0 cellphone:ml-3 text-zinc-500"
                 onClick={() => setStatus("all")}
             >
                 All
             </button>
             <button
-                className="tablet:p-5 cellphone:m-2 text-zinc-500"
+                className="tablet:p-5 tablet:m-0 cellphone:m-2 text-zinc-500"
                 onClick={() => setStatus("active")}
             >
                 Active
             </button>
             <button
-                className="tablet:p-5 cellphone:mr-2 text-zinc-500"
+                className="tablet:p-5 tablet:mr-0 cellphone:mr-2 text-zinc-500"
                 onClick={() => setStatus("completed")}
             >
                 Completed
